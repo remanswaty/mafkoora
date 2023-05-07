1f9d5a22283b60f35dd0eb12ae376d2f15d7c8dd
Reman Swati <yemshams@gmail.com>
2023-05-07 11:59:34 +0500

    first commit, basic setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+const { render } = require("react-dom");
+
+const App = () =>{
+    return(
+        <div>
+            <h1>Mafkoora site</h1>
+        </div>
+    );
+}
+
+render(<App/>, document.getElementById("root"));
